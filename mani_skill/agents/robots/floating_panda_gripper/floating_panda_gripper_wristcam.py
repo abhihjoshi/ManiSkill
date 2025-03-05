a5aceaf2f6763ed9a8b4d2028d13494b792d7b82
Abhishek Joshi <abhishekjoshinj@gmail.com>
2025-03-04 21:18:10 -0500

    updating floating panda

--- mani_skill/agents/robots/floating_panda_gripper/floating_panda_gripper_wristcam.py
+++ mani_skill/agents/robots/floating_panda_gripper/floating_panda_gripper_wristcam.py
@@ -1,129 +1,19 @@
-from copy import deepcopy
-
 import numpy as np
 import sapien
-import torch
 
 from mani_skill import PACKAGE_ASSET_DIR
-from mani_skill.agents.base_agent import BaseAgent, Keyframe
-from mani_skill.agents.controllers import *
 from mani_skill.agents.registration import register_agent
 from mani_skill.sensors.camera import CameraConfig
-from mani_skill.utils import common, sapien_utils
-from mani_skill.utils.structs.actor import Actor
+from mani_skill.utils import sapien_utils
 
+from .floating_panda_gripper import FloatingPandaGripper
 
 @register_agent()
-class FloatingPandaGripperWristcam(BaseAgent):
+class FloatingPandaGripperWristcam(FloatingPandaGripper):
+    """Floating Panda gripper with the real sense camera attached to gripper"""
+
     uid = "floating_panda_gripper_wristcam"
-    urdf_path = f"{PACKAGE_ASSET_DIR}/robots/panda/panda_v3_gripper.urdf"  # You can use f"{PACKAGE_ASSET_DIR}" to reference a urdf file in the mani_skill /assets package folder
-    urdf_config = dict(
-        _materials=dict(
-            gripper=dict(static_friction=2.0, dynamic_friction=2.0, restitution=0.0)
-        ),
-        link=dict(
-            panda_leftfinger=dict(
-                material="gripper", patch_radius=0.1, min_patch_radius=0.1
-            ),
-            panda_rightfinger=dict(
-                material="gripper", patch_radius=0.1, min_patch_radius=0.1
-            ),
-        ),
-    )
-    keyframes = dict(
-        open_facing_up=Keyframe(
-            qpos=[0, 0, 0, 0, 0, 0, 0.04, 0.04], pose=sapien.Pose(p=[0, 0, 0.5])
-        ),
-        open_facing_side=Keyframe(
-            qpos=[0, 0, 0, 0, np.pi / 2, 0, 0.04, 0.04], pose=sapien.Pose(p=[0, 0, 0.5])
-        ),
-        open_facing_down=Keyframe(
-            qpos=[0, 0, 0, 0, np.pi, 0, 0.04, 0.04], pose=sapien.Pose(p=[0, 0, 0.5])
-        ),
-    )
-    root_joint_names = [
-        "root_x_axis_joint",
-        "root_y_axis_joint",
-        "root_z_axis_joint",
-        "root_x_rot_joint",
-        "root_y_rot_joint",
-        "root_z_rot_joint",
-    ]
-    gripper_joint_names = [
-        "panda_finger_joint1",
-        "panda_finger_joint2",
-    ]
-    gripper_stiffness = 1e3
-    gripper_damping = 1e2
-    gripper_force_limit = 1000
-
-    ee_link_name = "panda_hand_tcp"
-
-    @property
-    def _controller_configs(self):
-        pd_ee_pose = PDEEPoseControllerConfig(
-            self.root_joint_names,
-            None,
-            None,
-            self.gripper_stiffness,
-            self.gripper_damping,
-            self.gripper_force_limit,
-            use_delta=False,
-            frame="base",
-            ee_link=self.ee_link_name,
-            urdf_path=self.urdf_path,
-            normalize_action=False,
-        )
-        pd_ee_pose_quat = deepcopy(pd_ee_pose)
-        pd_ee_pose_quat.rotation_convention = "quaternion"
-        pd_ee_delta_pose = PDEEPoseControllerConfig(
-            joint_names=self.root_joint_names,
-            pos_lower=-0.1,
-            pos_upper=0.1,
-            rot_lower=-0.1,
-            rot_upper=0.1,
-            stiffness=self.gripper_stiffness,
-            damping=self.gripper_damping,
-            force_limit=self.gripper_force_limit,
-            ee_link=self.ee_link_name,
-            urdf_path=self.urdf_path,
-        )
-        arm_pd_joint_pos = PDJointPosControllerConfig(
-            joint_names=self.root_joint_names,
-            lower=None,
-            upper=None,
-            stiffness=self.gripper_stiffness,
-            damping=self.gripper_damping,
-            force_limit=self.gripper_force_limit,
-            normalize_action=False,
-        )
-        arm_pd_joint_delta_pos = PDJointPosControllerConfig(
-            joint_names=self.root_joint_names,
-            lower=-0.1,
-            upper=0.1,
-            stiffness=self.gripper_stiffness,
-            damping=self.gripper_damping,
-            force_limit=self.gripper_force_limit,
-            use_delta=True,
-        )
-
-        gripper_pd_joint_pos = PDJointPosMimicControllerConfig(
-            self.gripper_joint_names,
-            -0.01,  # a trick to have force when the object is thin
-            0.04,
-            self.gripper_stiffness,
-            self.gripper_damping,
-            self.gripper_force_limit,
-        )
-        return dict(
-            pd_joint_delta_pos=dict(
-                root=arm_pd_joint_delta_pos, gripper=gripper_pd_joint_pos
-            ),
-            pd_joint_pos=dict(root=arm_pd_joint_pos, gripper=gripper_pd_joint_pos),
-            pd_ee_delta_pose=dict(root=pd_ee_delta_pose, gripper=gripper_pd_joint_pos),
-            pd_ee_pose=dict(root=pd_ee_pose, gripper=gripper_pd_joint_pos),
-            pd_ee_pose_quat=dict(root=pd_ee_pose_quat, gripper=gripper_pd_joint_pos),
-        )
+    urdf_path = f"{PACKAGE_ASSET_DIR}/robots/panda/panda_v3_gripper.urdf"
 
     @property
     def _sensor_configs(self):
@@ -131,59 +21,11 @@ class FloatingPandaGripperWristcam(BaseAgent):
             CameraConfig(
                 uid="hand_camera",
                 pose=sapien.Pose(p=[0, 0, 0], q=[1, 0, 0, 0]),
-                width=256,
-                height=256,
+                width=128,
+                height=128,
                 fov=np.pi / 2,
                 near=0.01,
                 far=100,
                 mount=self.robot.links_map["camera_link"],
             )
         ]
-        return []
-
-    def _after_init(self):
-        self.finger1_link = sapien_utils.get_obj_by_name(
-            self.robot.get_links(), "panda_leftfinger"
-        )
-        self.finger2_link = sapien_utils.get_obj_by_name(
-            self.robot.get_links(), "panda_rightfinger"
-        )
-        self.finger1pad_link = sapien_utils.get_obj_by_name(
-            self.robot.get_links(), "panda_leftfinger_pad"
-        )
-        self.finger2pad_link = sapien_utils.get_obj_by_name(
-            self.robot.get_links(), "panda_rightfinger_pad"
-        )
-        self.tcp = sapien_utils.get_obj_by_name(
-            self.robot.get_links(), self.ee_link_name
-        )
-
-    def is_grasping(self, object: Actor, min_force=0.5, max_angle=85):
-        """Check if the robot is grasping an object
-
-        Args:
-            object (Actor): The object to check if the robot is grasping
-            min_force (float, optional): Minimum force before the robot is considered to be grasping the object in Newtons. Defaults to 0.5.
-            max_angle (int, optional): Maximum angle of contact to consider grasping. Defaults to 85.
-        """
-        l_contact_forces = self.scene.get_pairwise_contact_forces(
-            self.finger1_link, object
-        )
-        r_contact_forces = self.scene.get_pairwise_contact_forces(
-            self.finger2_link, object
-        )
-        lforce = torch.linalg.norm(l_contact_forces, axis=1)
-        rforce = torch.linalg.norm(r_contact_forces, axis=1)
-
-        # direction to open the gripper
-        ldirection = self.finger1_link.pose.to_transformation_matrix()[..., :3, 1]
-        rdirection = -self.finger2_link.pose.to_transformation_matrix()[..., :3, 1]
-        langle = common.compute_angle_between(ldirection, l_contact_forces)
-        rangle = common.compute_angle_between(rdirection, r_contact_forces)
-        lflag = torch.logical_and(
-            lforce >= min_force, torch.rad2deg(langle) <= max_angle
-        )
-        rflag = torch.logical_and(
-            rforce >= min_force, torch.rad2deg(rangle) <= max_angle
-        )
-        return torch.logical_and(lflag, rflag)
